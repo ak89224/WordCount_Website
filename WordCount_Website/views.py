27eb3fe803f5ddcb632b7a8de0f98ec8d56047ff
ak89224 <52851630+ak89224@users.noreply.github.com>
2019-07-14 21:53:11 +0530

    Initial commit

---
+++ WordCount_Website/views.py
@@ -0,0 +1,31 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import operator
+
+
+def homepage(request):
+    return render(request, 'homepage.html')
+
+
+def count(request):
+    inputtext = request.GET['InputText']
+
+    wordlist = inputtext.split()
+    count = len(wordlist)
+
+    worddict = {}
+
+    for word in wordlist:
+        # increase the count of each word in dictionary if exists
+        if word in worddict:
+            worddict[word] += 1
+        else:
+            # Add the word to dictonary
+            worddict[word] = 1
+
+    sortedwords=sorted(worddict.items(),key=operator.itemgetter(1), reverse=True)
+    return render(request, 'count.html', {'inputtext': inputtext, 'count': count, 'worddict': sortedwords})
+
+
+def about(request):
+    return render(request, 'about.html')
